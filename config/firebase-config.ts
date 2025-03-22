c4f9e73823569bc1fee4ad006fe2f0dbfb76aa5e
manikng <manishsingh_23cs244@dtu.ac.in>
2025-03-22 15:54:36 +0530

    Refactor Firebase configuration to use environment variables and add Firestore and Realtime Database support

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,24 +1,26 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { createContext ,useContext } from "react";
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: import.meta.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export default app;
+export const FirebaseContext = createContext(null);
+
+export const useFirebase = ()=> useContext(FirebaseContext);
+
+
+const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
+ const database = getDatabase(app);
+
+ export  { app ,db ,database};
--- shared/database/firebase.tsx
+++ shared/database/firebase.tsx
@@ -1,21 +1,16 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { createContext, useContext, } from "react";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { createContext ,useContext } from "react";
 
-import {
-  getFirestore,
-} from "firebase/firestore";
-import {  getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAS6SmDT21sDYh2kSYgI5QOpd_U9HZjZGM",
-  authDomain: "rentit-b4555.firebaseapp.com",
-  databaseURL: "https://rentit-b4555-default-rtdb.firebaseio.com",
-  projectId: "rentit-b4555",
-  storageBucket: "rentit-b4555.firebasestorage.app",
-  messagingSenderId: "866324838598",
-  appId: "1:866324838598:web:0f618cb51aa1a9e227bfc7",
-  measurementId: "G-PLNG6E31P1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 
@@ -25,9 +20,45 @@ export const useFirebase = ()=> useContext(FirebaseContext);
 
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+ const db = getFirestore(app);
+ const database = getDatabase(app);
 
-const database = getDatabase(app);
+ export  { app ,db ,database};
 
 
-export  { app ,db ,database};
+
+
+
+// import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// import { createContext, useContext, } from "react";
+
+// import {
+//   getFirestore,
+// } from "firebase/firestore";
+// import {  getDatabase } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAS6SmDT21sDYh2kSYgI5QOpd_U9HZjZGM",
+//   authDomain: "rentit-b4555.firebaseapp.com",
+//   databaseURL: "https://rentit-b4555-default-rtdb.firebaseio.com",
+//   projectId: "rentit-b4555",
+//   storageBucket: "rentit-b4555.firebasestorage.app",
+//   messagingSenderId: "866324838598",
+//   appId: "1:866324838598:web:0f618cb51aa1a9e227bfc7",
+//   measurementId: "G-PLNG6E31P1",
+// };
+
+
+// export const FirebaseContext = createContext(null);
+
+// export const useFirebase = ()=> useContext(FirebaseContext);
+
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// const database = getDatabase(app);
+
+
+// export  { app ,db ,database};
